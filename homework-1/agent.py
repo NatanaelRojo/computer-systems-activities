c3609bee6dee02d3dfcd86141252b86152eabf9c
natanaelrojo <rojonatanael99@gmail.com>
2023-01-16 16:00:11 -0400

    homework 1

---
+++ homework-1/agent.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+
+class TwoArmedBandit():
+    def __init__(self, alpha=1, epsilon=None):
+        self.__arms = 2
+        self.alpha = alpha
+        self.epsilon = epsilon
+        self.reset()
+
+    def reset(self):
+        self.__action = 0
+        self.total_actions = 0
+        self.__reward = 0
+        self.total_reward = 0
+        self.__iteration = 0
+        self.values = np.zeros(self.__arms)
+
+    def update(self, action, reward):
+        self.__action = action
+        self.__reward = reward
+        self.__iteration += 1
+        self.values[action] = self.values[action] + \
+            self.alpha * (reward - self.values[action])
+        self.total_reward += self.values[action]
+
+    def get_action(self, mode):
+        if mode == 'random':
+            return self.__random()
+        elif mode == 'greedy':
+            return self.__greedy()
+        elif mode == "epsilon greedy":
+            return self.__epsilon_greedy()
+        self.total_actions += 1
+
+    def render(self):
+        print("Iteration: {}, Action: {}, Reward: {}, Values: {}, total reward: {}".format(
+            self.__iteration, self.__action, self.__reward, self.values, self.total_reward))
+
+    def __random(self):
+        return np.random.choice(self.__arms)
+
+    def __greedy(self):
+        return np.argmax(self.values)
+
+    def __epsilon_greedy(self):
+        epsilon = self.epsilon or 1 / (1 + self.total_actions)
+        p = np.random.random()
+
+        if p < epsilon:
+            return self.__random()
+        else:
+            return self.__greedy()
---
+++ homework-1/main.py
@@ -0,0 +1,27 @@
+import sys
+import gym_environments
+import gym
+from agent import TwoArmedBandit
+
+num_iterations = 100 if len(sys.argv) < 2 else int(sys.argv[1])
+version = "v0" if len(sys.argv) < 3 else sys.argv[2]
+
+print("Chose a action selector:")
+print("random")
+print("greedy")
+print("epsilon greedy")
+selector = input("Write a selector: ")
+
+
+env = gym.make(f"TwoArmedBandit-{version}")
+agent = TwoArmedBandit(0.1)
+
+env.reset(options={'delay': 1})
+
+for iteration in range(num_iterations):
+    action = agent.get_action(selector)
+    _, reward, _, _, _ = env.step(action)
+    agent.update(action, reward)
+    agent.render()
+
+env.close()
